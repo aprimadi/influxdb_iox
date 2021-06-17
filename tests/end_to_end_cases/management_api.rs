9ea4f38e9b290121437b72034fd3bf3ee501a5e0
Carol (Nichols || Goulding) <carol.nichols@integer32.com>
2021-06-17 16:53:32 -0400

    refactor: Rename field to not contain the type

--- tests/end_to_end_cases/management_api.rs
+++ tests/end_to_end_cases/management_api.rs
@@ -222,7 +222,7 @@ async fn test_create_get_update_database() {
             seconds: 2,
             nanos: 0,
         }),
-        write_buffer_connection_string: "".into(),
+        write_buffer_connection: "".into(),
     };
 
     client
