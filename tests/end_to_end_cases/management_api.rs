bfef5a2871deb13d102e6f260f020e93bb037421
Carol (Nichols || Goulding) <carol.nichols@integer32.com>
2021-06-17 17:06:12 -0400

    feat: Change write buffer connection rule to take either Writing or Reading connection info
    A database on one IOx server can, exclusively:
    
    - Not interact with Kafka at all
    - Send writes to Kafka
    - Read writes from Kafka
    
    Notably, a database on a particular server will never write *and* read from Kafka at the same time.

--- tests/end_to_end_cases/management_api.rs
+++ tests/end_to_end_cases/management_api.rs
@@ -222,7 +222,7 @@ async fn test_create_get_update_database() {
             seconds: 2,
             nanos: 0,
         }),
-        write_buffer_connection: "".into(),
+        write_buffer_connection: None,
     };
 
     client
